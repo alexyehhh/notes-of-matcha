2d4c74294d0a7be6cb4c023ac6e8068ec7da39e9
“Alex <alpyeh@ucsc.edu>
2026-03-18 01:07:37 -0700

    Match grid icon corner radius to nav buttons

--- src/imports/Group2.tsx
+++ src/imports/Group2.tsx
@@ -3,14 +3,14 @@ import svgPaths from "./svg-3vf1xlvk3k";
 export default function Group2() {
   return (
     <div className="relative size-full">
-      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 32 32">
+      <svg className="block size-full" fill="none" viewBox="0 0 32 32">
         <g clipPath="url(#clip0_18_15)" id="Group 2">
-          <rect fill="var(--fill-0, #342209)" height="31.4811" id="Rectangle 11" rx="2.67925" width="31.4811" />
+          <rect fill="var(--fill-0, #342209)" height="32" id="Rectangle 11" rx="6" width="32" />
           <path d={svgPaths.p3163100} fill="var(--fill-0, #EDDECF)" id="Union" />
         </g>
         <defs>
           <clipPath id="clip0_18_15">
-            <rect fill="white" height="31.4811" width="31.4811" />
+            <rect fill="white" height="32" rx="6" width="32" />
           </clipPath>
         </defs>
       </svg>
